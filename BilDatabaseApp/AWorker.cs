fe2c40491b70abdbb3e141fff0a368bcdba1ca2b
Peter Levinsky <pele@easj.dk>
2022-03-16 12:43:56 +0100

    Alle metoder lavet

--- BilDatabaseApp/AWorker.cs
+++ BilDatabaseApp/AWorker.cs
@@ -24,11 +24,35 @@ namespace BilDatabaseApp
                 Console.WriteLine(c);
             }
 
+            Console.WriteLine("Bil med reg nummer DD22334");
+            Console.WriteLine( GetByRegNr("DD22334"));
 
+            
 
             // opret bil
-            Car nyBil = new Car("DD22334", "Blå", false, "11223344");
-            Create(nyBil);
+            Console.WriteLine("Oprettet bil");
+            Car nyBil = new Car("ZZ22334", "Blå", false, "11223344");
+            Car c1 = Create(nyBil);
+            Console.WriteLine(c1);
+            Console.Write("Tryk return for at fortsætte ");
+            Console.ReadLine();
+
+            // modify
+            Console.WriteLine("Ændret bil");
+            nyBil.Colour = "Meget sort";
+            Car c2 = Modify(nyBil.RegNr, nyBil);
+            Console.WriteLine(c2);
+            Console.Write("Tryk return for at fortsætte ");
+            Console.ReadLine();
+
+            // slet bil
+            Console.WriteLine("Slettet bil");
+            Car c3 = Delete(nyBil.RegNr);
+            Console.WriteLine(c3);
+            Console.Write("Tryk return for at fortsætte ");
+            Console.ReadLine();
+
+
 
         }
 
@@ -76,7 +100,31 @@ namespace BilDatabaseApp
 
         public Car GetByRegNr(string regNr)
         {
-            throw new System.NotImplementedException();
+            String sql = "select * from Car where RegNr=@RegNr";
+
+            // opret forbindelse
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // åbner forbindelsen
+                connection.Open();
+
+                // opretter sql query
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                // indsæt værdierne
+                cmd.Parameters.AddWithValue("@RegNr", regNr);
+
+                // altid ved select
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                // læser alle rækker
+                while (reader.Read())
+                {
+                    Car car = ReadCar(reader);
+                    return car;
+                }
+            }
+
+            return null; // eller throw new KeyNotFoundException();
         }
 
         public Car Create(Car newCar)
@@ -114,12 +162,68 @@ namespace BilDatabaseApp
 
         public Car Delete(string regNr)
         {
-            throw new System.NotImplementedException();
+            Car deletedCar = GetByRegNr(regNr);
+
+            String sql = "delete from Car where RegNr=@RegNr";
+
+            // opret forbindelse
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // åbner forbindelsen
+                connection.Open();
+
+                // opretter sql query
+                SqlCommand cmd = new SqlCommand(sql, connection);
+
+                // indsæt værdierne
+                cmd.Parameters.AddWithValue("@RegNr", regNr);
+     
+
+                // altid ved Insert, update, delete
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows != 1)
+                {
+                    // fejl
+                }
+
+                return deletedCar;
+            }
         }
 
         public Car Modify(string regNr, Car modifiedCar)
         {
-            throw new System.NotImplementedException();
+            String sql = "update Car set RegNr=@RegNr, Colour=@Colour, El=@El, Ejer=@Ejer where RegNr=@UpdateRegNr";
+
+            // opret forbindelse
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // åbner forbindelsen
+                connection.Open();
+
+                // opretter sql query
+                SqlCommand cmd = new SqlCommand(sql, connection);
+
+                // indsæt værdierne
+                cmd.Parameters.AddWithValue("@RegNr", modifiedCar.RegNr);
+                cmd.Parameters.AddWithValue("@Colour", modifiedCar.Colour);
+                cmd.Parameters.AddWithValue("@El", modifiedCar.El);
+                cmd.Parameters.AddWithValue("@Ejer", modifiedCar.Ejer);
+                cmd.Parameters.AddWithValue("@UpdateRegNr", regNr);
+
+
+                // altid ved Insert, update, delete
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows != 1)
+                {
+                    // fejl
+                }
+
+                return modifiedCar;
+            }
+
+
         }
     }
 }
